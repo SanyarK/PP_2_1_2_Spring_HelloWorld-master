bfc84d379a2da00d2441164250c594a21a0526f8
SanyarK <102152149+SanyarK@users.noreply.github.com>
2022-05-17 16:54:46 +0500

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,27 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getMessage());
+
+        boolean compire = catBean1.equals(catBean2);
+        System.out.println(compire);
+
+        boolean compireHw = bean1.equals(bean2);
+        System.out.println(compireHw);
+    }
+}
